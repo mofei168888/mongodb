8f20cd5c9f733f95cca55a27457c584d8a44cdd4
Robin <379111601@qq.com>
2019-04-28 00:05:04 +0300

    keep run change

--- start.sh
+++ start.sh
@@ -6,6 +6,3 @@ mkdir -p /data/db/db-001
 
 # Run mongo replica sets using config files
 mongod --config /conf/mongo.conf  --fork
-
-
-docker run -idt --name=monitor-db -p 27017:27017  mongo --bind_ip_all
