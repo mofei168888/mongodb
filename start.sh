ade83ab3b887d2f45f688d7339cde2507bf8b166
Robin <379111601@qq.com>
2019-04-27 20:22:17 +0300

    add mkdir file

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 #centquant mongodb Rep Set
 
 # Create database directories
-mkdir /data/db/db-001
+mkdir -p /data/db/db-001
 
 # Run mongo replica sets using config files
 mongod --config /conf/mongo.conf
