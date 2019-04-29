0389454224c82b1fed58fd7613b0870e8e9fce72
Robin <379111601@qq.com>
2019-04-29 09:38:24 +0300

    bind_ip_all v1.0

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@
 mkdir -p /data/db/db-001
 
 # Run mongo replica sets using config files
-mongod --config /conf/mongo.conf  --fork
+mongod --config /conf/mongo.conf  --fork --bind_ip_all
 
 # Run mongo as the running process, this is required to keep the docker process running
 mongo
