77de52b4e40d38201e33d515282c6cd66f1831e9
Robin <379111601@qq.com>
2019-04-29 18:43:54 +0300

    add auth for mongodb

--- start.sh
+++ start.sh
@@ -3,9 +3,18 @@
 
 # Create database directories
 mkdir -p /data/db/db-001
+mkdir -p /path/to
+touch keyfile
 
 # Run mongo replica sets using config files
 mongod --config /conf/mongo.conf  --fork --bind_ip_all
 
 # Run mongo as the running process, this is required to keep the docker process running
 mongo
+use admin
+db.createUser(
+  {
+    user: "uxsoft",
+    pwd: "soFt10",
+    roles: [ { role: "userAdminAnyDatabase", db: "admin" }]
+  }）
