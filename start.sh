ae4f51c4e235f880533a58a7a8f7cf68cc506dd5
Robin <379111601@qq.com>
2019-04-30 09:43:46 +0300

    add auth for mongodb v2.0

--- start.sh
+++ start.sh
@@ -7,6 +7,6 @@ mkdir -p /path/to
 touch keyfile
 
 # Run mongo replica sets using config files
-mongod --config /conf/mongo.conf  --fork --bind_ip_all
+mongod --config /conf/mongo.conf  --fork
 
 # Run mongo as the running process, this is required to keep the docker process running
